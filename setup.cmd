5cb16dbbc54c60c6fb33570404f5d7fbe675b340
Marcin <muszynskimarcin@wp.pl>
2017-08-15 21:16:59 +1000

    Setup scripts

--- setup.cmd
+++ setup.cmd
@@ -1,5 +1,22 @@
-1. mkdir etc.
-2. virtualenv & source '' & cd src/
-3. pip install -r requirements.txt
-4. populate db
-5. run server
+@ECHO OFF
+cd %~dp0
+IF NOT EXIST %~dp0\venv (
+    echo Virtualenv directory not found.
+    pip install virtualenv
+    echo Setting up venv.
+    virtualenv venv
+) ELSE (
+    echo Virtualenv directory found.
+)
+echo Activating virtualenv.
+start venv/Scripts/activate
+echo Installing requirements.
+pip install -r requirements.txt
+echo Setting up mongoDB
+mongo < mongo_setup.js
+start /b mongod --dbpath %~dp0\mongodb
+cd src
+echo Populating database
+python manage.py populate_db
+echo Runserver on port 8000
+python manage.py runserver
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+cd `dirname "$0"`
 if [ "$(python -c 'import sys; print(sys.version_info[0])')" -lt 3 ]; then
     echo "Incompatible python version. Must be greater than 3."
     return 0
@@ -10,13 +10,20 @@ pwd
 if [[ -e `dirname "$0"`/venv ]]; then
     echo "Virtualenv directory found."
 else
+    echo "Virtualenv directory not found."
     pip install virtualenv
+    echo "Setting up venv"
     virtualenv venv
 fi
-source venv/Scripts/activate
+echo "Activating venv"
+source venv/bin/activate
+echo "Installing requirements."
 pip install -r requirements.txt
+echo "Setting up mongoDB"
 mongo < mongo_setup.js
 mongod --dbpath `dirname "$0"`/mongodb &
 cd src
+echo "Populating database"
 python manage.py populate_db
+echo "Runserver on port 8000"
 python manage.py runserver
